6cca92cd79eb0823e14ab7843f74b91b3c80d5f6
jesuscadev <jesuscadev@icloud.com>
2021-05-21 00:06:13 -0500

    Initial commit

---
+++ roller-coaster-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  roller-coaster-app
+//
+//  Created by Jesús Casillas on 21/05/21.
+//  Copyright © 2021 Jesús Casillas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ roller-coaster-appTests/roller_coaster_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  roller_coaster_appTests.swift
+//  roller-coaster-appTests
+//
+//  Created by Jesús Casillas on 21/05/21.
+//  Copyright © 2021 Jesús Casillas. All rights reserved.
+//
+
+import XCTest
+@testable import roller_coaster_app
+
+class roller_coaster_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
